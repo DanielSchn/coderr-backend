12f627fb3785a893c42628cbba7c4fb11b8c2370
root <root@instance-coderr.europe-west4-b.c.coderr-441514.internal>
2024-11-14 11:18:30 +0000

    cors settings

--- coderr/settings.py
+++ coderr/settings.py
@@ -29,7 +29,7 @@ TESTING = 'test' in sys.argv
 SECRET_KEY = 'django-insecure-99y)@ocf=1m-=^@^2(n9b-qze$ilu3s*j@ul0$l**n$=$i19qv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -56,20 +56,24 @@ INSTALLED_APPS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:5500',
-    'http://localhost:5500'
+    #'http://127.0.0.1:5500',
+    #'http://localhost:5500'
+    'http://coder.dschneider-dev.de',
+    'https://coderr.dschneider-dev.de'
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5500',
-    'http://localhost:5500'
+    #'http://127.0.0.1:5500',
+    #'http://localhost:5500',
+    'http://coder.dschneider-dev.de',
+    'https://coderr.dschneider-dev.de'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
